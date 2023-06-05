52ec5ff111dbbd4d07bd1f1b1f04342dab1f02d4
Juan M. Romano Siriani <jromano743@gmail.com>
2023-06-05 18:57:15 -0300

    Finish Sudoku Prototype

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -2,35 +2,42 @@ let numSelected = null;
 let tileSelected = null;
 let errors = 0;
 
-let board = [
-    "--74916-5",
-    "2---6-3-9",
-    "-----7-1-",
-    "-586----4",
-    "--3----9-",
-    "--62--187",
-    "9-4-7---2",
-    "67-83----",
-    "81--45---"
-]
+let gameBoard = [];
+let boardsolution = [];
 
-let solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
+// //EXAMPLE
+// let board = [
+//     "--74916-5",
+//     "2---6-3-9",
+//     "-----7-1-",
+//     "-586----4",
+//     "--3----9-",
+//     "--62--187",
+//     "9-4-7---2",
+//     "67-83----",
+//     "81--45---"
+// ]
+
+// let solution = [
+//     "387491625",
+//     "241568379",
+//     "569327418",
+//     "758619234",
+//     "123784596",
+//     "496253187",
+//     "934176852",
+//     "675832941",
+//     "812945763"
+// ]
 
 window.onload = function() {
     setGame();    
 }
 
 function setGame() {
+    //Set Board Game
+    setBoard();
+    
     //Digits
     for (let i = 1; i <= 9; i++) {
         let number = document.createElement("div");
@@ -46,8 +53,8 @@ function setGame() {
         for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-            if(board[r][c] != "-") {
-                tile.innerText = board[r][c];
+            if(gameBoard[r][c] != "0") {
+                tile.innerText = gameBoard[r][c];
                 tile.classList.add("tile-start");
             }
 
@@ -63,6 +70,17 @@ function setGame() {
             document.getElementById("board").append(tile);
         }
     }
+}
+
+function setBoard() {
+    do {
+        gameBoard = createBoard();
+        boardsolution = JSON.parse(JSON.stringify(gameBoard));
+        isResolved(boardsolution);
+        console.log("Try");
+    } while (!isResolved(boardsolution));
+    console.log("Board: ", gameBoard);
+    console.log("Solution: ", boardsolution);
 }
 
 function selectNumber() {
@@ -95,10 +113,130 @@ function selectTile() {
     }
 }
 
-function generateBoard() {
-
+//Create board
+function checkRow(board, row, value) {
+    return !board[row].includes(value);
 }
 
-function findSolve() {
+function checkColumn(board, column, value) {
+    for (let i = 0; i < 9; i++) {
+        if (board[i][column] === value) {
+        return false;
+        }
+    }
+    return true;
+}
+
+function checkRegion(board, row, column, value) {
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(column / 3) * 3;
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+            if (board[i][j] === value) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function createBoard() {
+    // Crear un tablero vacío
+    const board = Array.from({ length: 9 }, () => Array(9).fill(0));
     
+    // Generar valores iniciales aleatorios
+    const numValoresIniciales = Math.floor(Math.random() * (30 - 20 + 1)) + 20; // Cantidad de valores iniciales a establecer
+    
+    for (let count = 0; count < numValoresIniciales; count++) {
+        let row = Math.floor(Math.random() * 9);
+        let column = Math.floor(Math.random() * 9);
+        let value = Math.floor(Math.random() * 9) + 1;
+    
+        // Verificar si la celda ya tiene un valor asignado o si se repite en fila, columna o región
+        while (
+            board[row][column] !== 0 ||
+            !checkRow(board, row, value) ||
+            !checkColumn(board, column, value) ||
+            !checkRegion(board, row, column, value)
+        ) {
+            row = Math.floor(Math.random() * 9);
+            column = Math.floor(Math.random() * 9);
+            value = Math.floor(Math.random() * 9) + 1;
+        }
+    
+        // Establecer el valor en la celda
+        board[row][column] = value;
+    }
+
+    return board;
+}
+
+
+//ResolveBoard
+function isResolved(board) {
+    const isEmptyCell = findEmptyCell(board);
+    if (!isEmptyCell) {
+        return true; // El Sudoku está resuelto
+    }
+
+    const [row, column] = isEmptyCell;
+
+    for (let num = 1; num <= 9; num++) {
+        if (isValidNumber(board, row, column, num)) {
+            board[row][column] = num;
+
+            if (isResolved(board)) {
+                return true;
+            }
+
+            board[row][column] = 0; // Deshacer el número si no conduce a una solución válida
+        }
+    }
+
+    return false; // No se encontró ninguna solución
+}
+
+function findEmptyCell(board) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+        if (board[i][j] === 0) {
+            return [i, j];
+        }
+        }
+    }
+    return null; // No hay celdas vacías
+}
+
+function isValidNumber(board, row, column, num) {
+    return (
+        !existsInRow(board, row, num) &&
+        !existsInColumn(board, column, num) &&
+        !existsInRegion(board, row, column, num)
+    );
+}
+
+function existsInRow(board, row, num) {
+    return board[row].includes(num);
+}
+
+function existsInColumn(board, column, num) {
+    for (let i = 0; i < 9; i++) {
+        if (board[i][column] === num) {
+        return true;
+        }
+    }
+    return false;
+}
+
+function existsInRegion(board, row, column, num) {
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(column / 3) * 3;
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+        if (board[i][j] === num) {
+            return true;
+        }
+        }
+    }
+    return false;
 }
