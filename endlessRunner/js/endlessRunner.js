1c559a18010dd680b94975b89e7e64454e41fd81
Juan M. Romano Siriani <jromano743@gmail.com>
2023-06-19 21:42:13 -0300

    Finish mecanics

--- endlessRunner/js/endlessRunner.js
+++ endlessRunner/js/endlessRunner.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-
+// Player settings
 const player = {
     x: 200, 
-    y: 350, 
+    y: 550, 
     width: 50,
     height: 50,
     speed: 300, 
@@ -12,6 +12,16 @@ const player = {
     acceleration: 1.2, 
     image: new Image()
 };
+
+// Obstacles
+const obstacleWidth = 50;
+const obstacleHeight = 50;
+const obstacleSpeed = 7;
+const obstacleSpawnInterval = 500; // msec
+let obstacles = [];
+
+const obstacleImage = new Image();
+obstacleImage.src = './img/blueBall.png';
 
 // Load sprite
 player.image.src = './img/redBall.png';
@@ -28,6 +38,32 @@ document.addEventListener('keyup', function(event) {
     keys[event.code] = false;
 });
 
+function spawnObstacle() {
+    const obstacleX = Math.random() * (canvas.width - obstacleWidth);
+    const obstacleY = -100;
+    const obstacle = { x: obstacleX, y: obstacleY };
+  
+    obstacles.push(obstacle);
+}
+
+function updateObstacles() {
+    for (let i = 0; i < obstacles.length; i++) {
+        const obstacle = obstacles[i];
+        obstacle.y += obstacleSpeed;
+    
+        // Draw obstacle
+        context.drawImage(obstacleImage, obstacle.x, obstacle.y, obstacleWidth, obstacleHeight);
+    
+        // TODO: CheckCollision
+        console.log("Dibuja en: ", obstacle.x, obstacle.y);
+        // Delete obstacle
+        if (obstacle.y > canvas.height) {
+            obstacles.splice(i, 1);
+            i--;
+        }
+    }
+}
+
 
 function updateGame() {
     // Update velocity
@@ -42,11 +78,15 @@ function updateGame() {
     player.x += player.velocity;
     checkBorders(player, canvas);
 
+    
     // Clear screen
     context.clearRect(0, 0, canvas.width, canvas.height);
-
+    
     // Draw Player
     context.drawImage(player.image, player.x, player.y, player.width, player.height);
+    
+    // Update Obstacles
+    updateObstacles();
 
     // Update next frame
     requestAnimationFrame(updateGame);
@@ -72,7 +112,10 @@ function checkBorders(actor, container) {
     if (actor.y + actor.height > container.height) {
       actor.y = container.height - actor.height;
     }
-  }
+}
+
+// Spawn obstacle
+setInterval(spawnObstacle, obstacleSpawnInterval);
 
 // Game loop
 updateGame();
