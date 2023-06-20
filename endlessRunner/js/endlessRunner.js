6c56c9216d1148276d23325ff63594c610c94e67
Juan M. Romano Siriani <jromano743@gmail.com>
2023-06-19 21:14:31 -0300

    Basic Player movement

---
+++ endlessRunner/js/endlessRunner.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+
+const player = {
+    x: 200, 
+    y: 350, 
+    width: 50,
+    height: 50,
+    speed: 300, 
+    velocity: 0, 
+    acceleration: 1.2, 
+    image: new Image()
+};
+
+// Load sprite
+player.image.src = './img/redBall.png';
+
+// Key's pressed
+const keys = {};
+
+// Add events
+document.addEventListener('keydown', function(event) {
+    keys[event.code] = true;
+});
+
+document.addEventListener('keyup', function(event) {
+    keys[event.code] = false;
+});
+
+
+function updateGame() {
+    // Update velocity
+    if (keys['ArrowLeft']) {
+        player.velocity -= player.acceleration;
+    } else if (keys['ArrowRight']) {
+        player.velocity += player.acceleration;
+    }
+
+    // Friction
+    player.velocity *= 0.9;
+    player.x += player.velocity;
+
+    // Clear screen
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw Player
+    context.drawImage(player.image, player.x, player.y, player.width, player.height);
+
+    // Update next frame
+    requestAnimationFrame(updateGame);
+}
+
+// Game loop
+updateGame();
