06c151a6826be877e7aa4bb77e0d395ded942a7d
Juan M. Romano Siriani <jromano743@gmail.com>
2023-06-22 20:00:59 -0300

    Refactor code AND add sounds

---
+++ endlessRunner/js/actor.js
@@ -0,0 +1,111 @@
+class Actor {
+    constructor(x, y, width, height, speed, velocity, acceleration, image){
+        this._x = x;
+        this._y =  y;
+        this._width = width;
+        this._height = height;
+        this._speed = speed;
+        this._velocity = velocity;
+        this._acceleration = acceleration;
+        this._image = image;
+        this._tag = undefined;
+        this._draw = undefined;
+        this._update = undefined;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(newX) {
+        this._x = newX;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(newY) {
+        this._y = newY;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    set width(newWidth) {
+        this._width = newWidth;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set height(newHeight) {
+        this._height = newHeight;
+    }
+
+    get speed() {
+        return this._speed;
+    }
+
+    set speed(newSpeed) {
+        this._speed = newSpeed;
+    }
+
+    get velocity() {
+        return this._velocity;
+    }
+
+    set velocity(newVelocity) {
+        this._velocity = newVelocity;
+    }
+
+    get acceleration() {
+        return this._acceleration;
+    }
+
+    set acceleration(newAcceleration) {
+        this._acceleration = newAcceleration;
+    }
+
+    get image() {
+        return this._image;
+    }
+
+    set image(newImage) {
+        this._image = newImage;
+    }
+
+    get tag() {
+        return this._tag;
+    }
+
+    set tag(newTag) {
+        this._tag = newTag;
+    }
+
+    get draw() {
+        return this._draw;
+    }
+    
+    set draw(callback) {
+        if (typeof callback === 'function') {
+            this._draw = callback;
+        } else {
+            throw new Error('draw must be a function');
+        }
+    }
+
+    get update() {
+        return this._update;
+    }
+    
+    set update(callback) {
+        if (typeof callback === 'function') {
+            this._update = callback;
+        } else {
+            throw new Error('update must be a function');
+        }
+    }
+}
--- endlessRunner/js/endlessRunner.js
+++ endlessRunner/js/endlessRunner.js
@@ -2,35 +2,104 @@ const gameContainer = document.getElementById('container');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-console.log(`Canvas size ${canvas.width} ${canvas.height}`)
+const borderWidth = 1;
+const borderColor = 'black';
 
-// Player settings
-const player = {
-    x: 200, 
-    y: 550, 
-    width: 50,
-    height: 50,
-    speed: 300, 
-    velocity: 0, 
-    acceleration: 1.2, 
-    image: new Image()
-};
+context.lineWidth = borderWidth;
+context.strokeStyle = borderColor;
 
-// Obstacles
-const obstacleWidth = 50;
-const obstacleHeight = 50;
-const obstacleSpeed = 7;
+const TAGS = Object.freeze({
+    PLAYER: 'PLAYER',
+    ENEMY: 'ENEMY',
+    COLLECTABLE: 'COLLECTABLE',
+})
+
+
+/**
+ * GLOBAL VARIABLES
+ */
+let points = 0;
+
+// Spawner settings
 const obstacleSpawnInterval = 500; // msec
+const collectableSpawnInterval = 600; // msec
+
 let obstacles = [];
-
-const obstacleImage = new Image();
-obstacleImage.src = './img/blueBall.png';
-
-// Load sprite
-player.image.src = './img/redBall.png';
+let collectables = []
 
 // Key's pressed
 const keys = {};
+
+// Audio
+const gameOverSound = new Audio("./assets/audio/CONFIRM_1.wav");
+const pointSound = new Audio("./assets/audio/CLICK_DENY_6.wav");
+
+
+/**
+ * SETTINGS
+ */
+
+// PLAYER
+const player = new Actor(200, 550, 50, 50, 300, 0, 1.2, new Image());
+
+const updatePlayer = function(inputKeys){
+    // Input: Update player velocity
+    if (inputKeys['ArrowLeft']) {
+        this._velocity -= this._acceleration;
+    } else if (inputKeys['ArrowRight']) {
+        this._velocity += this._acceleration;
+    }
+
+    // Friction
+    this._velocity *= 0.9;
+    this._x += this._velocity;
+
+
+}
+
+const drawPlayer = function(ctx){
+    ctx.drawImage(this._image, this._x, this._y, this._width, this._height);
+}
+
+player.draw = drawPlayer;
+player.update = updatePlayer;
+player.image.src = './assets/img/redBall.png';
+player.tag = TAGS.PLAYER;
+
+
+// BASIC OBSTACLE
+const originalObstacle = new Actor(200, -100, 50, 50, 7, 0, 1.2, new Image());
+
+const updateObstacle = function(){
+    this._y += this._speed;
+}
+
+const drawObstacle = function(ctx){
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.drawImage(this._image, this._x, this._y, this._width, this._height);
+}
+
+originalObstacle.draw = drawObstacle;
+originalObstacle.update = updateObstacle;
+originalObstacle.image.src = './assets/img/blueBall.png';
+originalObstacle.tag = TAGS.ENEMY;
+
+// BASIC COLLECTABLE
+const originalCollectable = new Actor(200, -100, 50, 50, 5, 0, 1.2, new Image());
+
+const updateCollectable = function(){
+    this._y += this._speed;
+}
+
+const drawCollectable = function(ctx){
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.drawImage(this._image, this._x, this._y, this._width, this._height);
+}
+
+originalCollectable.draw = drawCollectable;
+originalCollectable.update = updateCollectable;
+originalCollectable.image.src = './assets/img/greenBall.png';
+originalCollectable.tag = TAGS.COLLECTABLE;
 
 // Add events
 document.addEventListener('keydown', function(event) {
@@ -41,25 +110,38 @@ document.addEventListener('keyup', function(event) {
     keys[event.code] = false;
 });
 
+/**
+ * FUNCTIONS
+ */
 function spawnObstacle() {
-    const obstacleX = Math.random() * (canvas.width - obstacleWidth);
+    const obstacleX = Math.random() * (canvas.width - originalObstacle.width);
     const obstacleY = -100;
-    const obstacle = { x: obstacleX, y: obstacleY };
+
+    const obstacle = Object.assign(Object.create(Object.getPrototypeOf(originalObstacle)), originalObstacle);
+    obstacle.x = obstacleX;
+    obstacle.y = obstacleY;
   
     obstacles.push(obstacle);
 }
 
-function updateObstacles() {
+function spawnCollectable() {
+    const collectableX = Math.random() * (canvas.width - originalCollectable.width);
+    const collectableY = -100;
+
+    const collectable = Object.assign(Object.create(Object.getPrototypeOf(originalCollectable)), originalCollectable);
+    collectable.x = collectableX;
+    collectable.y = collectableY;
+  
+    collectables.push(collectable);
+}
+
+function updateObstacles(ctx) {
     for (let i = 0; i < obstacles.length; i++) {
         const obstacle = obstacles[i];
-        obstacle.y += obstacleSpeed;
-    
-        // Draw obstacle
-        context.drawImage(obstacleImage, obstacle.x, obstacle.y, obstacleWidth, obstacleHeight);
-    
-        // TODO: CheckCollision
 
-        // Delete obstacle
+        obstacle.update();
+        obstacle.draw(ctx);
+        
         if (obstacle.y > canvas.height) {
             obstacles.splice(i, 1);
             i--;
@@ -67,32 +149,52 @@ function updateObstacles() {
     }
 }
 
+function updateCollectables(ctx) {
+    for (let i = 0; i < collectables.length; i++) {
+        const collectable = collectables[i];
 
-function updateGame() {
-    // Update velocity
-    if (keys['ArrowLeft']) {
-        player.velocity -= player.acceleration;
-    } else if (keys['ArrowRight']) {
-        player.velocity += player.acceleration;
+        collectable.update();
+        collectable.draw(ctx);
+        
+        if (collectable.y > canvas.height) {
+            collectables.splice(i, 1);
+            i--;
+        }
     }
+}
 
-    // Friction
-    player.velocity *= 0.9;
-    player.x += player.velocity;
+
+function gameLoop() {
+   
+
     checkBorders(player, canvas);
 
-    
-    // Clear screen
     context.clearRect(0, 0, canvas.width, canvas.height);
     
-    // Draw Player
-    context.drawImage(player.image, player.x, player.y, player.width, player.height);
-    
-    // Update Obstacles
-    updateObstacles();
+    //Player update
+    player.update(keys);
+    player.draw(context);
 
-    // Update next frame
-    requestAnimationFrame(updateGame);
+    obstacles.forEach((obstacle, index) => {
+        if(checkCollision(player, obstacle))
+        {
+            obstacles.splice(index, 1);
+        }
+    })
+
+    collectables.forEach((collectable, index) => {
+        if(checkCollision(player, collectable))
+        {
+            collectables.splice(index, 1);
+        }
+    })
+
+    //Update Objects
+    updateObstacles(context);
+    updateCollectables(context);
+
+    //Request frames
+    requestAnimationFrame(gameLoop);
 }
 
 function checkBorders(actor, container) {
@@ -102,8 +204,8 @@ function checkBorders(actor, container) {
     }
   
     // Right
-    if (actor.x + actor.width > container.width) {
-      actor.x = container.width - actor.width;
+    if (actor.x + actor.width > container.width + 5) {
+      actor.x = container.width - actor.width + 5;
     }
   
     // Above
@@ -121,21 +223,67 @@ function resizeCanvas() {
     canvas.width = gameContainer.offsetWidth;
     canvas.height = gameContainer.offsetHeight;
 }
+
+/**
+ * Check collision bounds form actorA Sprite with actorB Sprite
+ * @param {Object} actorA 
+ * @param {Object} actorB 
+ * @returns 
+ */
+function checkCollision(actorA, actorB) {
+    const actorALeft = actorA.x;
+    const actorARight = actorA.x + actorA.width;
+    const actorATop = actorA.y;
+    const actorABottom = actorA.y + actorA.height;
+  
+    const actorBLeft = actorB.x;
+    const actorBRight = actorB.x + actorB.width;
+    const actorBTop = actorB.y;
+    const actorBBottom = actorB.y + actorB.height;
+  
+    // Comprobar si los bordes se superponen
+    if (
+        actorALeft < actorBRight &&
+        actorARight > actorBLeft &&
+        actorATop < actorBBottom &&
+        actorABottom > actorBTop
+    ) {
+        checkCollisionActorTag(actorB.tag);
+        return true;
+    }
+  
+    // No hay colisión
+    return false;
+}
+
+function checkCollisionActorTag(tag){
+    if(tag == TAGS.ENEMY) {
+        console.log("Game Over")
+        gameOverSound.play();
+    }
+
+    if(tag == TAGS.COLLECTABLE) {
+        points++;
+        pointSound.play();
+        console.log(`Poinst: ${points}`);
+    }
+}
+
+
 // Spawn obstacle
 setInterval(spawnObstacle, obstacleSpawnInterval);
+setInterval(spawnCollectable, collectableSpawnInterval);
 
 resizeCanvas();
 // Game loop
-updateGame();
-
-
+gameLoop();
 
 // TODO:
 /*
     Add global width
-    Add points
+    Add points -
     Add visualization points
-    Add collision in objets
+    Add collision in objets - 
     Add menu buttons
     Add art, add music
 */
