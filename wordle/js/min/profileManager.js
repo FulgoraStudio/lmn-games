11d3f4ca6561e2788a68040c902e670c757a85f7
Juan M. Romano Siriani <jromano743@gmail.com>
2023-10-31 17:57:51 -0300

    add mileston system

---
+++ wordle/js/min/profileManager.js
@@ -0,0 +1,62 @@
+class ProfileManager {
+    static playerId = null;
+    static link = "";
+    static token = "";
+
+    static async GetProfileCode() {
+        this.initLink();
+        const playerIdMatch = this.link.match(/player_id=([0-9]+)/);
+
+        if (playerIdMatch && playerIdMatch[1]) {
+            this.playerId = playerIdMatch[1];
+        } else {
+            console.error("User ID not detected");
+        }
+    }
+
+    /**
+     * 
+     * @param {number} points 
+     * @param {number} milestone 
+     */
+    static async SaveNewPoint(points, milestone) {
+
+        if (this.playerId !== null) {
+            try {
+                const playerStats = {
+                    "juego_token": this.token,
+                    "user_id": this.playerId,
+                    "puntaje": points,
+                    "hitos": milestone,
+                };
+        
+                const response = await fetch('https://juegos.lmnjuegos.com/api/resultado', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(playerStats),
+                });
+        
+                if (response.status === 200) {
+                    console.log("New point saved successfully.");
+                } else if (response.status === 400) {
+                    console.error("The request contains invalid or incomplete data");
+                } else if (response.status === 401) {
+                    console.error("The API Key is invalid or was not provided");
+                } else if (response.status === 500) {
+                    console.error("An error occurred on our server. Please try again later.");
+                }
+
+            } catch (error) {
+                console.error("Error while saving the new point: ", error);
+            }
+        } else {
+            console.warn("No player ID available. Cannot save a new point.");
+        }
+    }
+
+    static initLink() {
+        this.link = window.location.href;
+    }
+}
